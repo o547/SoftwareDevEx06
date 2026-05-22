deecb46e73cd3f96195348e8f4745355490229a3
o547 <otashibaura@gmail.com>
2026-05-23 00:52:50 +0900

    チャットボットへの質問を更新

--- project06/static/toyosu_campus_navi/js/script.js
+++ project06/static/toyosu_campus_navi/js/script.js
@@ -33,6 +33,36 @@ function openSidebar(sidebarId,buttonId) {
   openButton.classList.remove("active");
 }
 
+//チャットボットに質問をする
+async function chatbotSubmit(){
+
+  const div = document.querySelector(".chatbot-conversation");
+  const chatbotInput = document.querySelector(".chatbot-input");
+  
+  //Ajax通信で返答を取得
+  const response = await fetch("/chatbot/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value,
+    },
+    body: JSON.stringify({
+      question: chatbotInput.value
+    })
+  });
+  const data = await response.json();
+  
+  //ユーザの質問文を描画
+  div.insertAdjacentHTML("beforeend", `<div class="chatbot-message user-message">${chatbotInput.value}</div>`);
+  
+  //テキストボックスをクリア
+  chatbotInput.value= "";
+  
+  //返答文を描画
+  div.insertAdjacentHTML("beforeend", `<div class="chatbot-message">${data.chatbot_response}</div>`);
+  // console.log(data.chatbot_response);
+}
+
 //サーバーから変数を受け取る
 const alert_message = JSON.parse(
   document.getElementById("alert_message").textContent
