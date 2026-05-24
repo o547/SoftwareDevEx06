a319691a413ba70bb315fd9677195444f7e93403
o547 <otashibaura@gmail.com>
2026-05-24 23:38:45 +0900

    ルーティング整備

--- project06/toyosu_campus_navi/views.py
+++ project06/toyosu_campus_navi/views.py
@@ -3,13 +3,120 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .navi import navi
 from .map import map
+
+from .processes import *
+
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
+from .models import *
 
-#経路案内のデモ画面
+import json
+from django.http import JsonResponse
+
+
+# フロントに送る情報をまとめる
+def create_info_to_send(request):
+    uset_info = LoginProcess().get_user_info(request)
+    alert_message = request.session.get("alert_message")
+    if alert_message:
+        del request.session["alert_message"]
+    return {
+        "alert_message": alert_message,
+        "is_login": uset_info["is_login"],
+        "username": uset_info["username"],
+        "is_superuser": uset_info["is_superuser"],
+        "language": uset_info["language"],
+    }
+
+
+# /
+class IndexView(View):
+    def get(self, request):
+        info_to_send = create_info_to_send(request)
+        return render(request, "toyosu_campus_navi/index.html", info_to_send)
+
+
+# /notice
+class NoticeView(View):
+    def get(self, request):
+        info_to_send = create_info_to_send(request)
+        return render(request, "toyosu_campus_navi/notice.html", info_to_send)
+
+
+# /notice/management
+class NoticeManagementView(View):
+    def get(self, request):
+        info_to_send = create_info_to_send(request)
+        return render(
+            request, "toyosu_campus_navi/notice_management.html", info_to_send
+        )
+
+
+# /notice/edit
+class NoticeEditView(View):
+    def get(self, request):
+        info_to_send = create_info_to_send(request)
+        return render(request, "toyosu_campus_navi/notice_edit.html", info_to_send)
+
+
+# /history
+class HistoryView(View):
+    def get(self, request):
+        info_to_send = create_info_to_send(request)
+        return render(request, "toyosu_campus_navi/history.html", info_to_send)
+
+
+# /user/login
+class UserLoginView(View):
+    def post(self, request):
+
+        login_id = request.POST["login-id"]
+        login_password = request.POST["login-password"]
+        login_method = request.POST["login-method"]
+
+        if login_method == "login":
+            is_login_success = LoginProcess().user_login(
+                request, login_id, login_password
+            )
+            if is_login_success:
+                return redirect("toyosu_campus_navi:history")
+        elif login_method == "regist":
+            LoginProcess().user_regist(request, login_id, login_password)
+
+        return redirect("toyosu_campus_navi:index")
+
+
+# /chatbot/submit
+class ChatBotView(View):
+    def post(self, request):
+
+        body = json.loads(request.body)
+        question = body["question"]
+        response = ChatBotProcess().reply_to_chat(request, question)
+
+        return JsonResponse({"chatbot_response": response})
+
+
+# /language/submit
+class languageView(View):
+    def post(self, request):
+
+        body = json.loads(request.body)
+        language = body["language"]
+        response = LoginProcess().save_language(request, language)
+        alert_message = request.session.get("alert_message")
+        if alert_message:
+            del request.session["alert_message"]
+        return JsonResponse({"alert_message": alert_message})
+
+
+# -------------------豊洲キャンパスナビ対象外 ここから-------------------
+
+
+# /demo/guide
+# 経路案内のデモ画面
 class DemoIndexView(View):
     def get(self, request):
         nodes = list(navi.nodes.keys())
@@ -63,7 +170,9 @@ class DemoIndexView(View):
             },
         )
 
-#地図データ作成用の画像座標取得アプリ
+
+# /plot
+# 地図データ作成用の画像座標取得アプリ
 class PlotView(View):
     def get(self, request):
         return render(
@@ -71,53 +180,28 @@ class PlotView(View):
             "toyosu_campus_navi/plot.html",
         )
 
-#メイン画面
-class IndexView(View):
+
+# /debug
+# ここを書き換えて、/deubugにアクセスする
+class DebugView(View):
     def get(self, request):
-        user_id = str(request.user)
-        login_button_text = "ログイン"
-        if(user_id != "AnonymousUser"):
-            login_button_text = "履歴"
-        alert_message = request.session.get("alert_message")
-        if(alert_message):            
-            print(alert_message)
-            del request.session["alert_message"]
-        return render(
-            request,
-            "toyosu_campus_navi/index.html",
-            {"user_id":user_id,
-             "alert_message":alert_message}
-        )
-
-#ユーザーのログイン及び新規作成
-class UserLoginView(View):
-    def post(self, request):
-        login_id = request.POST["login-id"]
-        login_password = request.POST["login-password"]
-        login_method = request.POST["login-method"]
-        if(login_method == "login"):
-            user = authenticate(request,username=login_id, password=login_password)
-            if user is not None:
-                #ログイン成功
-                login(request, user)
-            else:
-                request.session["alert_message"] = "ログインできませんでした"
-        elif(login_method == "create"):
-            if User.objects.filter(username=login_id).exists():
-                #ユーザーIDが既に存在している
-                request.session["alert_message"] = "そのIDは存在しています"
-                None
-            else:
-                #アカウントを新規作成する
-                User.objects.create_user(login_id,"",login_password)
-                user = authenticate(request,username=login_id, password=login_password)
-                login(request, user)
+        print("デバッグ内容")
         return redirect("toyosu_campus_navi:index")
+    
+            
 
 
+# -------------------豊洲キャンパスナビ対象外 ここまで-------------------
 
 
-demo_index = DemoIndexView.as_view()
 index = IndexView.as_view()
 user_login = UserLoginView.as_view()
+chatbot_submit = ChatBotView.as_view()
+language_submit = languageView.as_view()
+notice = NoticeView.as_view()
+notice_management = NoticeManagementView.as_view()
+notice_edit = NoticeEditView.as_view()
+history = HistoryView.as_view()
 plot = PlotView.as_view()
+demo_index = DemoIndexView.as_view()
+debug = DebugView.as_view()
